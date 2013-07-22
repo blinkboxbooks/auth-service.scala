ac172ca2922bc7f97f94c7af859bdee1a038af14
Greg Beech <greg@gregbeech.com>
2013-07-22 11:29:48 +0100

    Increased password hashing strength
    Also updated to latest gem dependencies

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source "http://gems.mobcastdev.local/"
+source "http://gems.mobcastdev.com/"
 source "https://rubygems.org"
 ruby "2.0.0"
 gemspec
--- lib/blinkbox/zuul/server/models/user.rb
+++ lib/blinkbox/zuul/server/models/user.rb
@@ -21,7 +21,7 @@ module Blinkbox::Zuul::Server
     def password=(password)  
       if password
         @password_length = password.length
-        self.password_hash = SCrypt::Password.create(password)
+        self.password_hash = SCrypt::Password.create(password, max_time: 0.2, max_mem: 16 * 1024 * 1024)
       else
         @password_length = 0
         # TODO: This should probably set password_has to nil but that raises an error...
