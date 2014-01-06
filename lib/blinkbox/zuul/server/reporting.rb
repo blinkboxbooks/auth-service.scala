b58d466c53cf0fd9ccdf5d6b00b045356487624c
Anargyros Akrivos <anargyros@blinkbox.com>
2014-01-06 13:35:21 +0000

    Changed exchange name for zuul events

--- lib/blinkbox/zuul/server/reporting.rb
+++ lib/blinkbox/zuul/server/reporting.rb
@@ -132,7 +132,7 @@ module Blinkbox
           unless defined?(@amqp)
             @amqp = { connection: Bunny.new(App.properties[:amqp_server_url]).start }
             @amqp[:channel] = @amqp[:connection].create_channel
-            @amqp[:exchange] = @amqp[:channel].fanout("Events.Outbound", durable: true)
+            @amqp[:exchange] = @amqp[:channel].fanout("Zuul.Events", durable: true)
           end
           @amqp[:exchange]
         end
