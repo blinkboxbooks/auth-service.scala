ef503a23c10cc119431c4da2128a1064bc736e8b
Greg Beech <greg@blinkbox.com>
2014-08-05 09:50:38 +0100

    Added tests for JSON messaging.

--- src/test/scala/com/blinkbox/books/auth/server/events/RabbitMqPublisherTests.scala
+++ src/test/scala/com/blinkbox/books/auth/server/events/RabbitMqPublisherTests.scala
@@ -1,18 +1,18 @@
 package com.blinkbox.books.auth.server.events
 
-import java.io.ByteArrayInputStream
 import java.nio.charset.StandardCharsets
 
 import com.blinkbox.books.auth.server.data
-import com.blinkbox.books.schemas.events.user.v2.{UserId, User}
+import com.blinkbox.books.schemas.events.client.v2.{ClientId, Client}
+import com.blinkbox.books.schemas.events.user.v2.{User, UserId}
 import com.blinkbox.books.test.MockitoSyrup
-import com.blinkbox.books.time.{StoppedClock, SystemTimeSupport}
+import com.blinkbox.books.time.StoppedClock
 import com.rabbitmq.client.{AMQP, Channel}
-import org.joda.time.{DateTimeZone, DateTime}
 import org.joda.time.format.ISODateTimeFormat
+import org.joda.time.{DateTime, DateTimeZone}
 import org.json4s.JsonAST.{JNull, JString}
+import org.json4s.jackson.Serialization
 import org.json4s.{CustomSerializer, DefaultFormats}
-import org.json4s.jackson.{Serialization, JsonMethods}
 import org.mockito.Matchers
 import org.mockito.Matchers._
 import org.mockito.Mockito._
@@ -34,23 +34,100 @@ class RabbitMqPublisherTests extends FunSuite with ScalaFutures with MockitoSyru
   implicit val clock = StoppedClock()
   implicit val formats = DefaultFormats + ISODateTimeSerializer
   
-  val dataUser = data.User(data.UserId(123), clock.now(), clock.now(), "john@example.org", "John", "Doe", "hash", allowMarketing = true)
-  val eventUser =  User(UserId(123), "john@example.org", "John", "Doe", allowMarketingCommunications = true)
-  
+  val userData = data.User(data.UserId(123), clock.now().minusDays(30), clock.now().minusDays(15), "john@example.org", "John", "Doe", "hash", allowMarketing = true)
+  val clientData = data.Client(data.ClientId(456), clock.now().minusDays(29), clock.now().minusDays(14), userData.id, "Test Client", "Apple", "iPhone", "iOS", "secret", isDeregistered = false)
+
+  val eventUser =  User(UserId(userData.id.value), userData.username, userData.firstName, userData.lastName, userData.allowMarketing)
+  val eventClient = Client(ClientId(clientData.id.value), clientData.name, clientData.brand, clientData.model, clientData.os)
+
   test("Declares an exchange with the correct parameters on instantiation") {
     val channel = mock[Channel]
     new RabbitMqPublisher(channel)
     verify(channel).exchangeDeclare("Shop", "headers", true)
   }
 
+  test("Sends client deregistered messages with the correct media type and payload") {
+    val channel = mock[Channel]
+    val publisher = new RabbitMqPublisher(channel)
+    whenReady(publisher.publish(ClientDeregistered(clientData))) { _ => verify(channel).basicPublish(
+      shopExchange,
+      noRoutingKey,
+      propertiesFor("application/vnd.blinkbox.books.events.client.deregistered.v2+json"),
+      messageOfType[Client.Registered] { msg => msg.timestamp == clientData.updatedAt && msg.client == eventClient })
+    }
+  }
+
+  test("Sends client registered messages with the correct media type and payload") {
+    val channel = mock[Channel]
+    val publisher = new RabbitMqPublisher(channel)
+    whenReady(publisher.publish(ClientRegistered(clientData))) { _ => verify(channel).basicPublish(
+      shopExchange,
+      noRoutingKey,
+      propertiesFor("application/vnd.blinkbox.books.events.client.registered.v2+json"),
+      messageOfType[Client.Registered] { msg => msg.timestamp == clientData.createdAt && msg.client == eventClient })
+    }
+  }
+
+  test("Sends client updated messages with the correct media type and payload") {
+    val newClientData = clientData.copy(updatedAt = clock.now().minusSeconds(1), name = "New Client")
+    val channel = mock[Channel]
+    val publisher = new RabbitMqPublisher(channel)
+    whenReady(publisher.publish(ClientUpdated(clientData, newClientData))) { _ => verify(channel).basicPublish(
+      shopExchange,
+      noRoutingKey,
+      propertiesFor("application/vnd.blinkbox.books.events.client.updated.v2+json"),
+      messageOfType[Client.Updated] { msg =>
+        msg.timestamp == newClientData.updatedAt &&
+        msg.client == Client(ClientId(newClientData.id.value), newClientData.name, newClientData.brand, newClientData.model, newClientData.os) &&
+        msg.previousDetails == eventClient })
+    }
+  }
+
+  test("Sends user authenticated messages with the correct media type and payload, when there is no client") {
+    val channel = mock[Channel]
+    val publisher = new RabbitMqPublisher(channel)
+    whenReady(publisher.publish(UserAuthenticated(userData, None))) { _ => verify(channel).basicPublish(
+      shopExchange,
+      noRoutingKey,
+      propertiesFor("application/vnd.blinkbox.books.events.user.authenticated.v2+json"),
+      messageOfType[User.Authenticated] { msg => msg.timestamp == clock.now() && msg.user == eventUser && msg.client == None })
+    }
+  }
+
+  test("Sends user authenticated messages with the correct media type and payload, when there is a client") {
+    val channel = mock[Channel]
+    val publisher = new RabbitMqPublisher(channel)
+    whenReady(publisher.publish(UserAuthenticated(userData, Some(clientData)))) { _ => verify(channel).basicPublish(
+      shopExchange,
+      noRoutingKey,
+      propertiesFor("application/vnd.blinkbox.books.events.user.authenticated.v2+json"),
+      messageOfType[User.Authenticated] { msg => msg.timestamp == clock.now() && msg.user == eventUser && msg.client == Some(eventClient) })
+    }
+  }
+
   test("Sends user registered messages with the correct media type and payload") {
     val channel = mock[Channel]
     val publisher = new RabbitMqPublisher(channel)
-    whenReady(publisher.publish(UserRegistered(dataUser))) { _ => verify(channel).basicPublish(
+    whenReady(publisher.publish(UserRegistered(userData))) { _ => verify(channel).basicPublish(
       shopExchange,
       noRoutingKey,
       propertiesFor("application/vnd.blinkbox.books.events.user.registered.v2+json"),
-      messageOfType[User.Registered] { msg => msg.timestamp == clock.now() && msg.user == eventUser })
+      messageOfType[User.Registered] { msg => msg.timestamp == userData.createdAt && msg.user == eventUser })
+    }
+  }
+
+  test("Sends user updated messages with the correct media type and payload") {
+    val newUserData = userData.copy(updatedAt = clock.now().minusSeconds(1), username = "fred@example.org", firstName = "Fred", lastName = "Bloggs")
+    val channel = mock[Channel]
+    val publisher = new RabbitMqPublisher(channel)
+    whenReady(publisher.publish(UserUpdated(userData, newUserData))) { _ => verify(channel).basicPublish(
+      shopExchange,
+      noRoutingKey,
+      propertiesFor("application/vnd.blinkbox.books.events.user.updated.v2+json"),
+      messageOfType[User.Updated] { msg =>
+        msg.timestamp == newUserData.updatedAt &&
+        msg.user == User(UserId(newUserData.id.value), newUserData.username, newUserData.firstName, newUserData.lastName, newUserData.allowMarketing) &&
+        msg.previousDetails == eventUser })
     }
   }
 
