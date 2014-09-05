48e7e8d942495b4982ab3592d4b98ef278852be9
Aldo Stracquadanio <aldos@blinkbox.com>
2014-09-05 14:29:28 +0100

    Remove unused macro import

--- src/test/scala/com/blinkbox/books/auth/server/service/SpecBase.scala
+++ src/test/scala/com/blinkbox/books/auth/server/service/SpecBase.scala
@@ -6,8 +6,6 @@ import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Millis, Span}
 import org.scalatest.{FlatSpec, Matchers}
 
-import scala.language.experimental.macros
-
 class SpecBase extends FlatSpec with env.SpecBase with Matchers with ScalaFutures with FailHelper {
   implicit override val patienceConfig = PatienceConfig(timeout = Span(3000, Millis), interval = Span(100, Millis))
 }
