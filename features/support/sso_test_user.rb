fee7dd0bd7f60d79d138c10919035cabeb8bf8f1
Jay McCure <jaym@blinkbox.com>
2014-09-24 12:52:36 +0100

    Tests: Add ability to SSO user to register on SSO and to log into books

--- features/support/sso_test_user.rb
+++ features/support/sso_test_user.rb
@@ -4,11 +4,33 @@ class SSOTestUser
                         :first_name,
                         :last_name,
                         :accepted_terms_and_conditions,
-                        :allow_marketing_communications
+                        :allow_marketing_communications,
+                        :scope
 
   attr_accessor :access_token,
                 :refresh_token,
                 :id
+
+  def generate_details(user_scope)
+    @first_name = random_name
+    @last_name = random_name
+    @username = random_email
+    @password = random_password
+    @accepted_terms_and_conditions = true
+    @allow_marketing_communications = true
+    @scope = 'sso:' + user_scope
+    self
+  end
+
+  def register
+    response = $sso.register_user(self)
+    if response.status == 200
+      token_info = ::JSON.parse(response.body)
+      @access_token = token_info["access_token"]
+      @refresh_token = token_info["refresh_token"]
+    end
+    response
+  end
 
   def authenticate(credentials)
     response = $sso.authenticate(credentials)
@@ -16,6 +38,19 @@ class SSOTestUser
       token_info = ::JSON.parse(response.body)
       @access_token = token_info["access_token"]
       @refresh_token = token_info["refresh_token"] if token_info["refresh_token"]
+    end
+    response
+  end
+
+  def authenticate_books(credentials)
+    response = $zuul.authenticate(credentials)
+    if response.status == 200
+      token_info = ::JSON.parse(response.body)
+      @access_token = token_info["access_token"]
+      @refresh_token = token_info["refresh_token"] if token_info["refresh_token"]
+      @id = token_info["user_id"]
+      @local_id = @id[/\d+$/]
+      @password = credentials["password"] if credentials["password"] # handle password resets
     end
     response
   end
