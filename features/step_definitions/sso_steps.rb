a198e89a615cd5c3f4e532cef9616c5f2b17f9c2
Jay McCure <jaym@blinkbox.com>
2014-09-24 12:51:36 +0100

    Tests: Add step defs to register via SSO, link in SSO and validate links in SSO

--- features/step_definitions/sso_steps.rb
+++ features/step_definitions/sso_steps.rb
@@ -1,3 +1,17 @@
+Given(/^I have an SSO account registered by (books|movies|music)$/) do |user_scope|
+  @me = SSOTestUser.new.generate_details(user_scope)
+  @me.register
+  expect(last_response.status).to eq(200)
+end
+
+When(/^I authenticate onto the books website$/) do
+  @me.authenticate_books(@credentials)
+end
+
+Then(/^my SSO account is linked to the service I registered with$/) do
+  $sso.link(@me.access_token)
+end
+
 Then(/^my details are correct in the SSO system$/) do
   @admin = SSOTestUser.new
   @admin.username = 'admin@blinkbox.com'
@@ -5,4 +19,5 @@ Then(/^my details are correct in the SSO system$/) do
   obtain_access_and_token_via_username_and_password(@admin)
   $sso.admin_find_user({ username: @me.username }, @admin.access_token)
   validate_sso_user_response
+  validate_sso_linked_to_books
 end
