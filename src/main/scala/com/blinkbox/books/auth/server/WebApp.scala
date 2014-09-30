1edbc3b887e931b54cec01776ff16a3e82dfc512
Aldo Stracquadanio <aldos@blinkbox.com>
2014-09-30 10:57:18 +0100

    CP-1910 Catch and log exceptions during app initialization

--- src/main/scala/com/blinkbox/books/auth/server/WebApp.scala
+++ src/main/scala/com/blinkbox/books/auth/server/WebApp.scala
@@ -3,13 +3,12 @@ package com.blinkbox.books.auth.server
 import akka.actor.Props
 import akka.util.Timeout
 import com.blinkbox.books.auth.server.cake._
-import com.blinkbox.books.auth.server.services.GeoIP
 import com.blinkbox.books.config.Configuration
 import com.blinkbox.books.logging.Loggers
 import com.blinkbox.books.spray._
 import com.blinkbox.books.time.SystemTimeSupport
+import com.typesafe.scalalogging.slf4j.StrictLogging
 import spray.can.Http
-import spray.http.RemoteAddress
 import spray.routing._
 
 import scala.concurrent.duration._
@@ -31,15 +30,21 @@ class WebService extends HttpServiceActor {
   def receive = runRoute(WebAppComponents.routes)
 }
 
-object WebApp extends App with Configuration with Loggers {
-  implicit val system = WebAppComponents.actorSystem
-  implicit val executionContext = system.dispatcher
-  implicit val startTimeout = Timeout(10.seconds)
+object WebApp extends App with Configuration with Loggers with StrictLogging {
+  try {
+    implicit val system = WebAppComponents.actorSystem
+    implicit val executionContext = system.dispatcher
+    implicit val startTimeout = Timeout(10.seconds)
 
-  sys.addShutdownHook(system.shutdown())
+    sys.addShutdownHook(system.shutdown())
 
-  val service = system.actorOf(Props[WebService], "web-service")
-  val localUrl = WebAppComponents.config.service.localUrl
+    val service = system.actorOf(Props[WebService], "web-service")
+    val localUrl = WebAppComponents.config.service.localUrl
 
-  HttpServer(Http.Bind(service, localUrl.getHost, port = localUrl.effectivePort))
+    HttpServer(Http.Bind(service, localUrl.getHost, port = localUrl.effectivePort))
+  } catch {
+    case ex: Throwable =>
+      logger.error("Error during initialization of the service", ex)
+      System.exit(1)
+  }
 }
