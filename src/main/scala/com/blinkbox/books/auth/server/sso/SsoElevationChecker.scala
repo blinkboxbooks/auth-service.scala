54f79282ad57d8b3269d87d8b4bae49888058ab0
Aldo Stracquadanio <aldos@blinkbox.com>
2014-10-06 15:44:46 +0100

    CP-1926 Fix SSO elevation checker to reject invalid tokens

--- src/main/scala/com/blinkbox/books/auth/server/sso/SsoElevationChecker.scala
+++ src/main/scala/com/blinkbox/books/auth/server/sso/SsoElevationChecker.scala
@@ -1,6 +1,7 @@
 package com.blinkbox.books.auth.server.sso
 
 import com.blinkbox.books.auth.{Elevation, ElevationChecker, User}
+import com.blinkbox.books.spray.InvalidTokenStatusException
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -9,9 +10,10 @@ class SsoElevationChecker(sso: Sso)(implicit executionContext: ExecutionContext)
     user.ssoAccessToken.map{
       t =>
         val token = SsoAccessToken(t)
-        val elevation = sso.sessionStatus(token).map(_.sessionElevation).map {
-          case Some(SsoTokenElevation.Critical) => Elevation.Critical
-          case _ => Elevation.Unelevated
+        val elevation = sso.sessionStatus(token).map(s => (s.status, s.sessionElevation)).map {
+          case (SsoTokenStatus.Valid, Some(SsoTokenElevation.Critical)) => Elevation.Critical
+          case (SsoTokenStatus.Valid, _) => Elevation.Unelevated
+          case (s, _) => throw new InvalidTokenStatusException(s"The provided token is not valid (status: $s)")
         }
         elevation.onSuccess{
           case Elevation.Critical => sso.extendSession(token)
